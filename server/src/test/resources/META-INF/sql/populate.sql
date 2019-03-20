77e157cd34bf207557b3a10f776cc011ed142bea
Elric Finot <finca@hotmail.fr>
2019-03-20 16:06:57 +0100

    tests utilisateur fait

--- server/src/test/resources/META-INF/sql/populate.sql
+++ server/src/test/resources/META-INF/sql/populate.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS utilisateur CASCADE;
 
-CREATE TABLE utilisateur (id BIGINT IDENTITY NOT NULL, login VARCHAR  UNIQUE  NOT NULL, password VARCHAR NOT NULL, PRIMARY KEY(id), role VARCHAR NOT NULL);
+CREATE TABLE utilisateur (id BIGINT IDENTITY NOT NULL, login VARCHAR  UNIQUE  NOT NULL, password VARCHAR NOT NULL, email VARCHAR NOT NULL, PRIMARY KEY(id), role VARCHAR NOT NULL);
 ALTER TABLE utilisateur ADD CONSTRAINT CHK_User CHECK (role='admin' OR role='user');
 
-INSERT INTO utilisateur (id, login, password, role) VALUES (1, 'axel', 'axel', 'admin');
-INSERT INTO utilisateur (id, login, password, role) VALUES (2, 'mervo', 'mervo', 'user');
-INSERT INTO utilisateur (id, login, password, role) VALUES (3, 'jonas', 'jonas', 'user');
-INSERT INTO utilisateur (id, login, password, role) VALUES (4, 'toto', 'toto', 'user');
+INSERT INTO utilisateur (id, login, password, email, role) VALUES (1, 'axel', 'axel', '', 'admin');
+INSERT INTO utilisateur (id, login, password, email, role) VALUES (2, 'mervo', 'mervo', '', 'user');
+INSERT INTO utilisateur (id, login, password, email, role) VALUES (3, 'jonas', 'jonas', '', 'user');
+INSERT INTO utilisateur (id, login, password, email, role) VALUES (4, 'toto', 'toto', '', 'user');
