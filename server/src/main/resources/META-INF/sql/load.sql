a775453860fc26f91d17c24d7b18ab1abbc68b0a
Elric Finot <finca@hotmail.fr>
2019-03-21 08:50:40 +0100

    ajout de la bdd pour les events complexe

--- server/src/main/resources/META-INF/sql/load.sql
+++ server/src/main/resources/META-INF/sql/load.sql
@@ -2,3 +2,17 @@ INSERT INTO utilisateur (id, login, password, email, role) VALUES (1, 'axel', 'a
 INSERT INTO utilisateur (id, login, password, email, role) VALUES (2, 'mervo', 'mervo', 'mervo@mail.com', 'user');
 INSERT INTO utilisateur (id, login, password, email, role) VALUES (3, 'jonas', 'jonas', 'jonas@mail.com', 'user');
 INSERT INTO utilisateur (id, login, password, email, role) VALUES (4, 'toto', 'toto', 'toto@mail.com', 'user');
+
+INSERT INTO evenement (id, nom, date, place, heure) VALUES (1, 'Premier Cours', '2019-03-20', 10, '15:00:00');
+INSERT INTO evenement (id, nom, date, place, heure) VALUES (2, 'Deuxieme Cours', '2019-03-21', 11, '10:30:00');
+INSERT INTO evenement (id, nom, date, place, heure) VALUES (3, 'Troisieme Cours', '2019-03-22', 12, '09:30:00');
+INSERT INTO evenement (id, nom, date, place, heure) VALUES (4, 'Quatrieme Cours', '2019-03-23', 13, '10:00:00');
+
+INSERT INTO evenementInscrits(idevent, iduser) VALUES (1, 1);
+INSERT INTO evenementInscrits(idevent, iduser) VALUES (1, 2);
+INSERT INTO evenementInscrits(idevent, iduser) VALUES (2, 3);
+INSERT INTO evenementInscrits(idevent, iduser) VALUES (2, 4);
+INSERT INTO evenementInscrits(idevent, iduser) VALUES (3, 1);
+INSERT INTO evenementInscrits(idevent, iduser) VALUES (3, 3);
+INSERT INTO evenementInscrits(idevent, iduser) VALUES (3, 4);
+INSERT INTO evenementInscrits(idevent, iduser) VALUES (4, 3);
--- server/src/main/resources/META-INF/sql/populate.sql
+++ server/src/main/resources/META-INF/sql/populate.sql
@@ -1,17 +1,29 @@
 DROP TABLE IF EXISTS utilisateur CASCADE;
 DROP TABLE IF EXISTS evenement CASCADE;
+DROP TABLE IF EXISTS evenementInscrits CASCADE;
 
 CREATE TABLE utilisateur (id BIGINT IDENTITY NOT NULL, login VARCHAR  UNIQUE  NOT NULL, password VARCHAR NOT NULL, email VARCHAR UNIQUE NOT NULL, role VARCHAR NOT NULL, PRIMARY KEY(id));
+CREATE TABLE evenement (id BIGINT IDENTITY NOT NULL, nom VARCHAR NOT NULL, date DATE, heure TIME, place INT NOT NULL, PRIMARY KEY(id));
+CREATE TABLE evenementInscrits(idevent IDENTITY NOT NULL, iduser IDENTITY NOT NULL, FOREIGN KEY(idevent) REFERENCES evenement(id), FOREIGN KEY(iduser) REFERENCES utilisateur(id));
 ALTER TABLE utilisateur ADD CONSTRAINT CHK_User CHECK (role='admin' OR role='user');
+
+
 
 INSERT INTO utilisateur (id, login, password, email, role) VALUES (1, 'axel', 'axel', 'axel@mail.com', 'admin');
 INSERT INTO utilisateur (id, login, password, email, role) VALUES (2, 'mervo', 'mervo', 'mervo@mail.com', 'user');
 INSERT INTO utilisateur (id, login, password, email, role) VALUES (3, 'jonas', 'jonas', 'jonas@mail.com', 'user');
 INSERT INTO utilisateur (id, login, password, email, role) VALUES (4, 'toto', 'toto', 'toto@mail.com', 'user');
 
-CREATE TABLE evenement (id BIGINT IDENTITY NOT NULL, nom VARCHAR NOT NULL, date DATE, heure TIME, PRIMARY KEY(id));
+INSERT INTO evenement (id, nom, date, place, heure) VALUES (1, 'Premier Cours', '2019-03-20', 10, '15:00:00');
+INSERT INTO evenement (id, nom, date, place, heure) VALUES (2, 'Deuxieme Cours', '2019-03-21', 11, '10:30:00');
+INSERT INTO evenement (id, nom, date, place, heure) VALUES (3, 'Troisieme Cours', '2019-03-22', 12, '09:30:00');
+INSERT INTO evenement (id, nom, date, place, heure) VALUES (4, 'Quatrieme Cours', '2019-03-23', 13, '10:00:00');
 
-INSERT INTO evenement (id, nom, date, heure) VALUES (1, 'Premier Cours', '2019-03-20', '15:00:00');
-INSERT INTO evenement (id, nom, date, heure) VALUES (2, 'Deuxieme Cours', '2019-03-21', '10:30:00');
-INSERT INTO evenement (id, nom, date, heure) VALUES (3, 'Troisieme Cours', '2019-03-22', '09:30:00');
-INSERT INTO evenement (id, nom, date, heure) VALUES (4, 'Quatrieme Cours', '2019-03-23', '10:00:00');
+INSERT INTO evenementInscrits(idevent, iduser) VALUES (1, 1);
+INSERT INTO evenementInscrits(idevent, iduser) VALUES (1, 2);
+INSERT INTO evenementInscrits(idevent, iduser) VALUES (2, 3);
+INSERT INTO evenementInscrits(idevent, iduser) VALUES (2, 4);
+INSERT INTO evenementInscrits(idevent, iduser) VALUES (3, 1);
+INSERT INTO evenementInscrits(idevent, iduser) VALUES (3, 3);
+INSERT INTO evenementInscrits(idevent, iduser) VALUES (3, 4);
+INSERT INTO evenementInscrits(idevent, iduser) VALUES (4, 3);
