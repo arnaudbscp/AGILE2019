7315aa4d309926d92adfdbfd7436f81cce0f3bb7
Elric Finot <finca@hotmail.fr>
2019-03-22 16:29:56 +0100

    limite réelle

--- server/src/main/java/fr/ulille/iut/pizzaland/ressources/EvenementRessource.java
+++ server/src/main/java/fr/ulille/iut/pizzaland/ressources/EvenementRessource.java
@@ -103,6 +103,7 @@ public class EvenementRessource {
                 .filter(e -> e.getNom().equals(nom))
                 .collect(Collectors.toList())
                 .get(0);
+        if(ee.getReservations().size() == ee.getPlace()) return Response.status(Response.Status.LENGTH_REQUIRED).entity("Le cours est plein\nj").build();
         try {
             ee.getReservations().add(dataAccess.getUserByLogin(login));
             dataAccess.updateEvent(ee);
